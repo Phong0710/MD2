dfa361d960203ce3634be7604c69e839cdaa8583
phong0710 <phonglh071099@gmail.com>
2023-02-15 17:11:12 +0700

    BT3_Triangle

--- Week_1/Kế Thừa/BT3_TK-TKhaiTriangle.ts
+++ Week_1/Kế Thừa/BT3_TK-TKhaiTriangle.ts
@@ -1,45 +1,83 @@
 class Shape {
-    protected name:string;
-    protected color:string;
+    private _color: string;
+    private _filled: boolean;
 
-    constructor(name: string, color: string) {
-        this.name = name;
-        this.color = color;
+    constructor(color: string, filled: boolean) {
+        this._color = color;
+        this._filled = filled;
+    }
+
+    get color(): string {
+        return this._color;
+    }
+
+    set color(value: string) {
+        this._color = value;
+    }
+
+    get filled(): boolean {
+        return this._filled;
+    }
+
+    set filled(value: boolean) {
+        this._filled = value;
+    }
+
+    toString(): string {
+        if (this._filled === true) {
+            return `A Shape with color of ${this._color} and filled.`
+        }else{
+            return `A Shape with color of ${this._color} and not filled.`
+        }
     }
 }
-class Triagle extends Shape{
-    private _AB:number;
-    private _BC:number;
-    private _AC:number;
+class Triangle extends Shape{
+    private _slide1: number;
+    private _slide2: number;
+    private _slide3: number;
 
-    constructor(name: string, color: string, AB: number=3, BC: number=4, AC: number=6) {
-        super(name, color);
-        this._AB = AB;
-        this._BC = BC;
-        this._AC = AC;
-    }
-
-    get AB(): number {
-        return this._AB;
+    constructor(color: string, filled: boolean, slide1: number = 3, slide2: number = 4, slide3: number = 6) {
+        super(color, filled);
+        this._slide1 = slide1;
+        this._slide2 = slide2;
+        this._slide3 = slide3;
     }
 
-    get BC(): number {
-        return this._BC;
+    check():any{
+        if(this._slide1 + this._slide2 > this._slide3 && this._slide1 + this._slide3 > this._slide2 && this._slide2 + this._slide3 > this._slide1) {
+            return "Triangle";
+        }
     }
 
-    get AC(): number {
-        return this._AC;
-    }
-    getPerimeter(){
-        return (this.AB+this.AC+this.BC)
-    }
-    P(){
-        return this.getPerimeter()/2
-    }
-    getArea():number{
-        return Math.sqrt(this.P()*(this.P()-this.AB)*(this.P()-this.BC)*(this.P()-this.AC))
+    get slide1(): number {
+        return this._slide1;
     }
 
+    get slide2(): number {
+        return this._slide2;
+    }
+
+    get slide3(): number {
+        return this._slide3;
+    }
+
+    getPerimeter(): number {
+        if(this.check() === "Triangle") {
+            return this._slide1 + this._slide2 + this._slide3;
+        } else {
+            return 0;
+        }
+    }
+
+    getArea(): number {
+        if(this.check() === 'Triangle') {
+            let p = this.getPerimeter() / 2;
+            return Math.sqrt(p * (p - this._slide1) * (p - this._slide2) * (p - this._slide3));
+        } else {
+            return 0;
+        }
+    }
 }
-let a=new Triagle("Tam Giac 1","red")
-console.log(a.getArea())
+let triangle1 = new Triangle("red", true)
+console.log(triangle1.getPerimeter());
+console.log(triangle1.getArea());
