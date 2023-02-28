38837095ce060d19aa5619cbe59927adfe21b1a9
phong0710 <phonglh071099@gmail.com>
2023-02-28 14:01:43 +0700

    TH1

---
+++ Week_3/Xử lý ngoại lệ & Debug/TH1_Try-Catch.ts
@@ -0,0 +1,12 @@
+let num = 5;
+let de_num = 0;
+try{
+    // if(de_num==0){
+    //     throw "Divide by zero error"
+    // } else {
+    //     let sol = num/de_num
+    // }
+    eval(" alert('ES6 Exception Handling')")
+} catch (e){
+    console.log("Error : "+ e );
+}
