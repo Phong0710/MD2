f895305d5da1351d6d2131de9f80b775ac278a97
phong0710 <phonglh071099@gmail.com>
2023-03-01 17:24:39 +0700

    BT2

--- Week_3/String_Regex/BT2.ts
+++ Week_3/String_Regex/BT2.ts
@@ -1,16 +1,15 @@
 class EmailRegex {
 
-    private EMAIL_REGEX: RegExp = /^[ACP]+[0-9]{4}[G-M]+$/;
+    private EMAIL_REGEX: RegExp = /^[0-9]{2,}[-][0-9]{10}$/;
 
     public validate(regex: string): boolean {
         return this.EMAIL_REGEX.test(regex);
     }
 }
 let emailRegex = new EmailRegex();
-let validEmail: string[] = ["C0318G", "M0318G", "P0323A"];
+let validEmail: string[] = ["84-0978489648", "a8222-22222"];
 
 for (let email of validEmail) {
     let isvalid: boolean = emailRegex.validate(email);
     console.log("Email is " + email + " is valid: " + isvalid);
 }
-
